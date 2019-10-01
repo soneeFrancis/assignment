b088ba68c7cf5071759afaaf985354c1449796a7
soneeFrancis <55840412+soneeFrancis@users.noreply.github.com>
2019-10-01 14:18:03 +0530

    Add files via upload

---
+++ assignment/Booking/models.py
@@ -0,0 +1,53 @@
+# Create your models here.
+from django.db import models
+
+
+# Create your models here.
+
+class Bus(models.Model):
+    bus_name = models.CharField(max_length=100)
+    driver_name=models.CharField(max_length=100,null=False)
+    source = models.CharField(max_length=100)
+    dest = models.CharField(max_length=100)
+    nos = models.DecimalField(decimal_places=0, max_digits=2)
+    rem = models.DecimalField(decimal_places=0, max_digits=2)
+    price = models.DecimalField(decimal_places=2, max_digits=6)
+    date = models.DateField()
+    time = models.TimeField()
+
+    def __str__(self):
+        return self.bus_name
+
+
+class User(models.Model):
+    user_id = models.AutoField(primary_key=True)
+    email = models.EmailField()
+    name = models.CharField(max_length=30)
+    password = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.email
+
+
+class Book(models.Model):
+    BOOKED = 'B'
+    CANCELLED = 'C'
+
+    TICKET_STATUSES = ((BOOKED, 'Booked'),
+                       (CANCELLED, 'Cancelled'),)
+    email = models.EmailField()
+    name = models.CharField(max_length=100)
+    userid =models.DecimalField(decimal_places=0, max_digits=2)
+    busid=models.DecimalField(decimal_places=0, max_digits=2)
+    bus_name = models.CharField(max_length=100)
+    source = models.CharField(max_length=100)
+    dest = models.CharField(max_length=100)
+    nos = models.DecimalField(decimal_places=0, max_digits=2)
+    price = models.DecimalField(decimal_places=2, max_digits=6)
+    date = models.DateField()
+    time = models.TimeField()
+    status = models.CharField(choices=TICKET_STATUSES, default=BOOKED, max_length=100)
+
+    def __str__(self):
+        return self.email
+
---
+++ assignment/readMe.txt
@@ -0,0 +1,11 @@
+# Bus-reservation-system-in-python-django-2.1
+A simple bus reservation system using django 2.1 in Python
+
+This is a pretty simple implementation of a bus reservation system in Pyhton Django 2.1.
+In this project, the users can:
+1. Find bus
+2. Book bus
+3. Cancel bus
+
+Python Version: 3.6.2
+django version: 2.2.2
